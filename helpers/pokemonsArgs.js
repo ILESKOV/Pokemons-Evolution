a5d6b1459144ebacc1f16e8725fdad4246c03397
Ivan Leskov <103837756+ILESKOV@users.noreply.github.com>
2022-09-12 08:25:10 +0200

    pokemons contract arguments for verifying

--- helpers/pokemonsArgs.js
+++ helpers/pokemonsArgs.js
@@ -1,6 +1,7 @@
 module.exports = [
     process.env.MINT_POKEMON_FEE,
     process.env.UPGRADE_IN_LEVEL_FEE,
+    process.env.MAX_SUPPLY,
     process.env.LEVEL_ADDRESS,
     process.env.STONES_ADDRESS,
 ]
